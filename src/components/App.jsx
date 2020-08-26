9d334e9cc6801e19eee36c93d7e07f78d38ae0ac
Raphael Discky <zundria.putra@gmail.com>
2020-08-26 13:23:58 +0700

    Add new note to an array

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,22 +1,22 @@
-import React from "react";
+import React, { useState } from "react";
 import Header from "./Header";
 import Footer from "./Footer";
 import Note from "./Note";
 import CreateArea from "./CreateArea";
 
 function App() {
+  const [notes, setNotes] = useState([]);
 
-function addNote(note) {
-
-}
+  function addNote(newNote) {
+    setNotes((prevNotes) => {
+      return [...prevNotes, newNote];
+    });
+  }
 
   return (
     <div>
       <Header />
-      <CreateArea 
-        onAdd={addNote}
-
-      />
+      <CreateArea onAdd={addNote} />
       <Note key={1} title="Note title" content="Note content" />
       <Footer />
     </div>
--- src/components/CreateArea.jsx
+++ src/components/CreateArea.jsx
@@ -1,19 +1,18 @@
 import React, { useState } from "react";
 
 function CreateArea(props) {
-
   const [note, setNote] = useState({
     title: "",
     content: "",
   });
 
   function handleChange(event) {
-    const {name, value} = event.target;
-    setNote(prevNote => {
+    const { name, value } = event.target;
+    setNote((prevNote) => {
       return {
         ...prevNote,
-        [name]: value
-      }; 
+        [name]: value,
+      };
     });
   }
 
