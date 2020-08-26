5faa249d75380f6c9f44fdcc52f25b86bf01e5ab
Raphael Discky <zundria.putra@gmail.com>
2020-08-26 20:21:03 +0700

    Styling with Material-UI

--- src/components/Note.jsx
+++ src/components/Note.jsx
@@ -1,8 +1,8 @@
 import React from "react";
+import DeleteIcon from "@material-ui/icons/Delete";
 
 function Note(props) {
-
-  function handleClick() {  
+  function handleClick() {
     props.onDelete(props.id);
   }
 
@@ -10,7 +10,9 @@ function Note(props) {
     <div className="note">
       <h1>{props.title}</h1>
       <p>{props.content}</p>
-      <button onClick={handleClick}>DELETE</button>
+      <button onClick={handleClick}>
+        <DeleteIcon />
+      </button>
     </div>
   );
 }
