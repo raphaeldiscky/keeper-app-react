505798524571951fab71ac7a12e0d3124622190f
Raphael Discky <zundria.putra@gmail.com>
2020-08-26 13:29:50 +0700

    Take array and render separate Note components for eact item

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -17,7 +17,9 @@ function App() {
     <div>
       <Header />
       <CreateArea onAdd={addNote} />
-      <Note key={1} title="Note title" content="Note content" />
+      {notes.map((noteItem) => {
+        return <Note title={noteItem.title} content={noteItem.content} />;
+      })}
       <Footer />
     </div>
   );
